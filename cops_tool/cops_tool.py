ac2eb7bf6dab6577c3b6569b35fe8fa51306eec7
Gre7g Luterman <greg.luterman@synapse-wireless.com>
2018-08-17 09:53:20 -0500

    Lower case letters and a battery

--- cops_tool/cops_tool.py
+++ cops_tool/cops_tool.py
@@ -12,21 +12,28 @@ def on_startup():
 
     init_display(USE_SPI)
 
-    print_8x8("Best neighbors:")
-    set_xy(1, 1)
-    print_8x8("09a39f -27dB")
-    set_xy(1, 2)
-    print_8x8("0ba154 -45dB")
-    set_xy(1, 3)
-    print_8x8("05ef18 -87dB")
-
-
-@setHook(HOOK_1S)
-def on_1s(t):
-    x = random() % 4
+    print_8x8("abcdefghijklm")
     set_xy(0, 1)
-    print_8x8("*" if x == 0 else " ")
+    print_8x8("nopqrstuvwxyz")
     set_xy(0, 2)
-    print_8x8("*" if x == 1 else " ")
+    print_8x8("\x00\x01\x02\x01\x03\x01\x04\x01\x05\x01\x06\x01\x07\x01")
     set_xy(0, 3)
-    print_8x8("*" if x == 2 else " ")
+    print_8x8("\x07\x08\x07\x09\x07\x0a\x07\x0b\x07\x0c")
+    # print_8x8("Best neighbors:")
+#     set_xy(1, 1)
+#     print_8x8("09a39f -27dB")
+#     set_xy(1, 2)
+#     print_8x8("0ba154 -45dB")
+#     set_xy(1, 3)
+#     print_8x8("05ef18 -87dB")
+#
+#
+# @setHook(HOOK_1S)
+# def on_1s(t):
+#     x = random() % 4
+#     set_xy(0, 1)
+#     print_8x8("*" if x == 0 else " ")
+#     set_xy(0, 2)
+#     print_8x8("*" if x == 1 else " ")
+#     set_xy(0, 3)
+#     print_8x8("*" if x == 2 else " ")
